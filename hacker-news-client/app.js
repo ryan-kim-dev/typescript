b50d0b4035d64c0e0069eb1011e7b9f26d7d418a
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-03-11 23:07:30 +0900

    Feat: Implement basic router

--- hacker-news-client/app.js
+++ hacker-news-client/app.js
@@ -1,5 +1,4 @@
 const container = document.querySelector('#root');
-const content = document.createElement('div');
 
 // * 1. AJAX 요청
 const ajax = new XMLHttpRequest();
@@ -18,39 +17,49 @@ function getData(url) {
 }
 
 // * 2. 가져온 데이터 UI에 표시
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul');
+function renderNewsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
+  newsList.push('<ul>');
 
-// * 3. hashChange 이벤트 감지
-window.addEventListener('hashchange', function () {
-  // 특정 글의 id 받아오기
-  // location 객체는 주소와 관련된 다양한 정보를 제공한다.
+  for (let i = 0; i < newsFeed.length; i++) {
+    newsList.push(`
+    <li>
+      <a href="#${newsFeed[i].id}">
+      ${newsFeed[i].title} (${newsFeed[i].comments_count}
+      </a>
+    </li>
+    `);
+  }
+
+  newsList.push('</ul>');
+
+  container.innerHTML = newsList.join('');
+}
+
+function renderNewsDetail() {
+  // 특정 글의 id 받아오기 - location 객체는 주소와 관련된 다양한 정보를 제공한다.
   const contentId = location.hash.substr(1); // # 제거를 위해 substr
 
   // 특정 게시글 내용 받아오기
   const newsContent = getData(CONTENT_URL.replace('@id', contentId));
 
-  // 화면에 표시
-  const title = document.createElement('h1');
-  title.textContent = newsContent.title;
-  content.appendChild(title);
-});
-
-for (let i = 0; i < newsFeed.length; i++) {
-  // 마크업 계층구조 확인의 용이성을 위해 DOM API(ex: createElement) 사용을 최대한 자제하도록 수정한 예시
-  const div = document.createElement('div'); // innerHTML로 li, a 태그를 끼워넣기 위한 임시 div
-
-  div.innerHTML = `
-  <li>
-    <a href="#${newsFeed[i].id}">
-    ${newsFeed[i].title} (${newsFeed[i].comments_count}
-    </a>
-  </li>
+  // SPA처럼 선택한 글만 화면에 보여지도록 리팩토링
+  container.innerHTML = `
+  <h1>${newsContent.title}</h1>
+  
+  <div>
+  <a href="#">목록으로</a>
+  </div>
   `;
-
-  // ul의 자식으로 li만 존재하도록 불필요한 li를 감싼 div 대신 내부의 li만 선택
-  ul.appendChild(div.firstElementChild); // ul.appendChildren[0] 과 동일
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+// * 3. 라우터
+function router() {
+  const routePath = location.hash;
+  if (!routePath) return renderNewsFeed(); // 최초 렌더링 시에는 전체 목록 화면으로
+
+  return renderNewsDetail();
+}
+window.addEventListener('hashchange', router);
+router();

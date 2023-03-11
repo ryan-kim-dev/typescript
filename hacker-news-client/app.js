ef6a91bb638770bbfa6d805108b4236137619511
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-03-11 22:13:06 +0900

    Refactor: Deduplicate ajax code
    - Make a data fetching function to remove duplicated ajax call

--- hacker-news-client/app.js
+++ hacker-news-client/app.js
@@ -4,19 +4,21 @@ const content = document.createElement('div');
 // * 1. AJAX 요청
 const ajax = new XMLHttpRequest();
 
-// 1.1 데이터 가져오기
 // api 요청 주소, 페이지 번호 등의 값은 바뀔 가능성이 있기 때문에 변수로 빼둔다
 const INDEX = 1;
 const NEWS_URL = `https://api.hnpwa.com/v0/news/${INDEX}.json`;
 const CONTENT_URL = `https://api.hnpwa.com/v0/item/@id.json`;
 
-ajax.open('GET', NEWS_URL, false);
-ajax.send(); // send 까지 해야 가져온다
+// 1.1 데이터 가져오기
+function getData(url) {
+  ajax.open('GET', url, false);
+  ajax.send(); // send 까지 해야 가져온다
 
-// 1.2 응답으로 받은 JSON데이터를 JS로 변환
-const newsFeed = JSON.parse(ajax.response);
+  return JSON.parse(ajax.response);
+}
 
 // * 2. 가져온 데이터 UI에 표시
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
 // * 3. hashChange 이벤트 감지
@@ -24,11 +26,10 @@ window.addEventListener('hashchange', function () {
   // 특정 글의 id 받아오기
   // location 객체는 주소와 관련된 다양한 정보를 제공한다.
   const contentId = location.hash.substr(1); // # 제거를 위해 substr
-  // 특정 게시글로 라우팅
-  ajax.open('GET', CONTENT_URL.replace('@id', contentId), false);
-  ajax.send();
+
   // 특정 게시글 내용 받아오기
-  const newsContent = JSON.parse(ajax.response);
+  const newsContent = getData(CONTENT_URL.replace('@id', contentId));
+
   // 화면에 표시
   const title = document.createElement('h1');
   title.textContent = newsContent.title;
